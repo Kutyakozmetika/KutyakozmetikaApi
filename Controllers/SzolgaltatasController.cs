d1daff25cac4ebc1cab372e0822d9cd40d69f91e
krisz501@sulid.hu <krisz501@sulid.hu>
2022-04-11 13:28:01 +0200

    SzolgaltatasController létrejötte

---
+++ Controllers/SzolgaltatasController.cs
@@ -0,0 +1,23 @@
+﻿using KutyakozmetikaApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KutyakozmetikaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SzolgaltatasController : ControllerBase
+    {
+
+        private kutyakozmetikaContext context;
+
+        public SzolgaltatasController(kutyakozmetikaContext kutyakozmetikaContext)
+        {
+            context = kutyakozmetikaContext;
+        }
+    }
+}
